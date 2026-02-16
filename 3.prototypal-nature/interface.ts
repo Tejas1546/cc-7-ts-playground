0449a33d6ab8376f1d9057b4654269341edd6b83
shripada <shripada.hebbar@codecraft.co.in>
2026-02-16 21:43:57 +0530

    prototype examples

---
+++ 3.prototypal-nature/interface.ts
@@ -0,0 +1,27 @@
+// * interface is another mechanism to create an object type.
+// * differs from the type alias, where an interface can be extended by another interface, whereas
+// * you can not extend a type from another type
+// * We can open an interface again and few more properties and thus can augment an existing interface.
+interface Flight {
+  operator: string;
+  origin: { airport: string; latitude: number; longitude: number };
+  destination: { airport: string; latitude: number; longitude: number };
+  date: string;
+  price: number;
+}
+
+// * One can derive from existing interface
+interface PassengerFlight extends Flight {
+  make: string;
+  countryOfOrigin: string;
+}
+
+// * One can augment existing interface by re opening the declaration and can add more fields
+interface Flight {
+  supersonic: boolean;
+
+  displayInfo(): string;
+}
+
+// ! Exercise : Create an instance of Flight.
+// ! Exercise: Create an instance of PassengerFlight. How do we implement displayInfo?
